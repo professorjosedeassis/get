ce7f1495c974ba03e8d820ad4f18d788481974a3
Professor José de Assis <josedeassisfilho@gmail.com>
2023-11-08 21:59:36 -0300

    update files

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# get
+![GitHub](https://img.shields.io/github/license/professorjosedeassis/get)
+![Website](https://img.shields.io/website?url=https%3A%2F%2Fprofessorjosedeassis.github.io%2Fget%2F)
+![W3C Validation](https://img.shields.io/w3c-validation/html?targetUrl=https%3A%2F%2Fprofessorjosedeassis.github.io%2Fget%2F)
+
+<p>
+    <a href="https://jigsaw.w3.org/css-validator/check/referer">
+        <img style="border:0;width:88px;height:31px"
+            src="https://jigsaw.w3.org/css-validator/images/vcss"
+            alt="CSS válido!" />
+    </a>
+</p>
+
+# GET
 Calculadora do gasto energético total.
--- get.js
+++ get.js
@@ -1,19 +1,15 @@
 /**
- * APP Health
+ * APP GET
  * @author Professor José de Assis
  * https://joseassis.com.br
  */
 
-// imc (índice de massa corporal)
-// fcm (frequência cardiaca máxima)
-// tmb (taxa metabólica basal)
 let peso, altura, idade, imc, fcm, tmb
 
 function calcular() {
     idade = frmIMC.txtIdade.value
     peso = frmIMC.txtPeso.value
     altura = frmIMC.txtAltura.value
-    //validação do formulário
     if (frmIMC.txtIdade.value === "") {
         alert("Preencha a idade")
         frmIMC.txtIdade.focus()
@@ -29,7 +25,6 @@ function calcular() {
         alert("Selecione o nível de atividade")
         frmIMC.nivel.focus()
     } else {
-        //IMC
         imc = peso / (altura * altura)
         document.getElementById("imc").innerHTML = (`IMC: ${imc.toFixed(2)}`)
         if (imc < 18.5) {
@@ -51,24 +46,16 @@ function calcular() {
             document.getElementById("status").innerHTML = "Obesidade extrema"
             document.getElementById("grafico").src = "icons/obesidadeExtrema.png"
         }
-        //FCM - Fórmula de Tanaka
         fcm = 208 - (0.7 * idade)
         document.getElementById("freq").innerHTML = fcm
-        //TMB - Fórmula de Harris Benedict
-        //variáveis locais para capturar o conteúdo da lista (vetor)
         let select = document.getElementById("atividade")
-        let opcaoValor = Number(select.options[select.selectedIndex].value) //valor da lista
-        //let opcao = select.options[select.selectedIndex].text //texto da lista
-        //alert(opcao) //apoio  verificação do valor
-        //fórmula para homens
+        let opcaoValor = Number(select.options[select.selectedIndex].value)
         if (document.getElementById("m").checked === true) {
             tmb = (66 + (13.7 * peso) + (5 * (altura * 100) - (6.8 * idade))) * opcaoValor
         }
-        //fórmula para mulheres 
         if (document.getElementById("f").checked === true) {
             tmb = (655 + (9.6 * peso) + (1.8 * (altura * 100) - (4.7 * idade))) * opcaoValor
         }
-        //resultado final
         document.getElementById("calorias").innerHTML = tmb.toFixed(2)
     }
 }
